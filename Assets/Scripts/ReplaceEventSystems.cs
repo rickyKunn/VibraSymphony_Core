000bbdcabd4680cf1d425aac1a8cdbe45318abd9
rickyKunn <kobayashiritsuki@gmail.com>
2025-04-28 10:20:02 +0900

    add setting

--- Assets/Scripts/ReplaceEventSystems.cs
+++ Assets/Scripts/ReplaceEventSystems.cs
@@ -1,82 +1,90 @@
-#if UNITY_EDITOR
-
-using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using System.Linq;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
-public class ReplaceEventSystems
+public static class ReplaceEventSystems
 {
     const float InitialHeight = 1.6f;
 
-    /// <summary> 初期化メソッド</summary>
+    // シーンロード直後に必ず呼ばれる（ビルド後も有効）
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     static void Init()
     {
-        // VR用シーンでなさそうならreturn
-        if (GameObject.FindObjectOfType<OVRCameraRig>() == null) { return; }
+        // OVRCameraRig がなければ VR シーンじゃない → 処理しない
+        if (Object.FindObjectOfType<OVRCameraRig>() == null)
+            return;
 
-        Replace();
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
+        DoReplace();
+        SceneManager.sceneLoaded += (scene, mode) => DoReplace();
     }
 
-    /// <summary> シーン読み込み完了時に呼び出されるメソッド</summary>
-    static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    static void DoReplace()
     {
-        Replace();
-    }
-
-    /// <summary> シーン上のVR向け設定を変換するメソッド</summary>
-    static void Replace()
-    {
-        // カメラ位置の調整
-        var ovrManager = GameObject.FindObjectOfType<OVRManager>();
-        if (ovrManager != null && ovrManager.trackingOriginType == OVRManager.TrackingOrigin.FloorLevel)
+        // (1) FloorLevel トラッキング時のカメラ高さ補正
+        var ovrMgr = Object.FindObjectOfType<OVRManager>();
+        if (ovrMgr != null
+            && ovrMgr.trackingOriginType == OVRManager.TrackingOrigin.FloorLevel
+            && Camera.main != null)
         {
             Camera.main.transform.position += Vector3.up * InitialHeight;
         }
 
-        // カメラにキーボード操作用コンポーネントを追加(必要なければ削除)
-        Camera.main.gameObject.AddComponent<EditorCamera>();
+        // (2) EditorCamera はエディター時のみ追加したい場合
+#if UNITY_EDITOR
+        if (Camera.main != null
+            && Camera.main.gameObject.GetComponent<EditorCamera>() == null)
+        {
+            Camera.main.gameObject.AddComponent<EditorCamera>();
+        }
+#endif
 
+        // (3) EventSystem と Canvas の差し替え・追加処理
         ReplaceEventSystem();
     }
 
-    /// <summary> EventSystemに関するシーン上のVR向け設定を変換するメソッド</summary>
     static void ReplaceEventSystem()
     {
-        // OVRInputModuleを無効にし、StandaloneInputModuleを有効に
-        var eventSystem = GameObject.FindObjectOfType<EventSystem>();
-        if (eventSystem.GetComponent<StandaloneInputModule>() == null)
+        // A) EventSystem の入力モジュール差し替え
+        var es = Object.FindObjectOfType<EventSystem>();
+        if (es != null)
         {
-            eventSystem.GetComponent<OVRInputModule>().enabled = false;
-            eventSystem.gameObject.AddComponent<StandaloneInputModule>();
-            Debug.Log("変換成功: OVRInputModule -> StandaloneInputModule");
-        }
-        // Project中の全Canvasコンポーネントの中からAssets以下でないもの(= Hierarchy上のもの)を全て取得
-        var canvases = Resources.FindObjectsOfTypeAll<Canvas>()
-            .Where(c => AssetDatabase.GetAssetOrScenePath(c).Contains(".unity")).ToArray();
+            var ovrInput = es.GetComponent<OVRInputModule>();
+            if (ovrInput != null)
+                ovrInput.enabled = false;
 
-        // GraphicRaycasterがあれば有効に、なければ追加
-        foreach (var canvas in canvases)
-        {
-            var raycasters = canvas.gameObject.GetComponents<GraphicRaycaster>().ToList();
-            if (raycasters.Exists(r => r.GetType() == typeof(GraphicRaycaster)))
+            if (es.GetComponent<StandaloneInputModule>() == null)
             {
-                raycasters.ForEach(r => r.enabled = true);
+                es.gameObject.AddComponent<StandaloneInputModule>();
+                Debug.Log("Replaced OVRInputModule → StandaloneInputModule");
             }
-            else
+        }
+
+        // B) Canvas の GraphicRaycaster を有効化 or 追加
+        if (SceneManager.GetActiveScene().name == "StartScene")
+        {
+            var parent = GameObject.Find("CanvasFollowManager");
+            var canvas = parent.GetComponentsInChildren<Canvas>(includeInactive: true);
+            foreach (var cv in canvas)
             {
-                canvas.gameObject.AddComponent<GraphicRaycaster>();
-                Debug.Log($"変換成功: GraphicRaycaster added to {canvas.name}");
+                cv.gameObject.AddComponent<GraphicRaycaster>();
+                Debug.Log($"[ReplaceEventSystems] Added GraphicRaycaster to Canvas '{cv.name}'");
+
+                // if (gr == null)
+                // {
+                //     cv.gameObject.AddComponent<GraphicRaycaster>();
+                //     Debug.Log($"[ReplaceEventSystems] Added GraphicRaycaster to Canvas '{cv.name}'");
+                // }
+                // else if (!gr.enabled)
+                // {
+                //     gr.enabled = true;
+                //     Debug.Log($"[ReplaceEventSystems] Enabled existing GraphicRaycaster on Canvas '{cv.name}'");
+                // }
             }
         }
     }
 }
-
-#endif
-
